d5f6b11dd69be5a24e4d6b2cbd13b0b4ff7b95db
Subwaytime <leon.l@nophase.de>
2021-02-10 15:49:37 +0100

    Update Option Type Interface
    Add ignoreDuplicates
    Add depth and deep
    Update Paths to relative ones

--- src/types.ts
+++ src/types.ts
@@ -2,49 +2,65 @@
  * Library options.
  */
 export interface Options {
-    /**
-     * Path to the project Directory
-     * @default './src/'
-     */
-    path: string
+	/**
+	 * Relative path to the project Directory
+	 * @default 'src'
+	 */
+	path: string;
 
-    /**
-     * Path to log the generated Aliases
-     * @default './src/logs/''
-     */
+	/**
+	 * Relative path to log the generated Aliases
+	 * @default 'src/logs'
+	 */
 
-    log_path: string
+	log_path: string;
 
-    /**
-     * Prefix Symbol for the Aliases
-     * @default '@'
-     */
-    prefix: string
+	/**
+	 * Prefix Symbol for the Aliases
+	 * @default '@'
+	 */
+	prefix: string;
 
-    /**
-     * Add a Leading Slash
-     * This makes it look more similiar to Vite 1
-     * @default false
-     */
+	/**
+	 * Allow Searching for Subdirectories
+	 * @default true
+	 */
+	deep: boolean;
 
-    addLeadingSlash: boolean
+	/**
+	 * Search Depthlevel for Subdirectories
+	 * @default 1
+	 */
+	depth: number;
 
-    /**
-     * Allow creating a Logger File
-     * @default false
-     */
+	/**
+	 * Add a Leading Slash
+	 * This makes it look more similiar to Vite 1
+	 * @default false
+	 */
+	addLeadingSlash: boolean;
 
-    allowLogging: boolean
+	/**
+	 * Allow creating a Logger File
+	 * @default false
+	 */
+	allowLogging: boolean;
 
-    /**
-     * Allow global project Directory alias
-     * @default true
-     */
-    allowGlobalAlias: boolean
+	/**
+	 * Allow global project Directory alias
+	 * @default true
+	 */
+	allowGlobalAlias: boolean;
 
-    /**
-     * Root path of Vite project
-     * @default 'process.cwd()'
-     */
-    root: string
+	/**
+	 * Ignore Error on Duplicate Folders
+	 * @default false
+	 */
+	ignoreDuplicates: boolean
+
+	/**
+	 * Root path of Vite project
+	 * @default 'process.cwd()'
+	 */
+	root: string;
 }
