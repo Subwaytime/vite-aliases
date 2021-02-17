a9d4406237276ccff3bd2f7e0fbe4f6e51340d61
Subwaytime <leon.l@nophase.de>
2021-02-17 22:46:52 +0100

    Seperate Directory Function
    Will be used for Watcher later

---
+++ src/fs/glob.ts
@@ -0,0 +1,28 @@
+import slash from 'slash';
+import fg from 'fast-glob';
+
+import { log } from '../utils';
+import type { Options } from '../types';
+
+
+/**
+ * Return all folders from the project directory
+ * @param options
+ */
+
+export function getDirectories(options: Partial<Options> = {}) {
+	const { path, deep , root, depth } = options;
+
+	const directories = fg.sync(deep ? `${path}/**/*` : `${path}/*`, {
+		ignore: ['node_modules'],
+		onlyDirectories: true,
+		cwd: root,
+		deep: depth
+	});
+
+	if (!directories.length) {
+		log('No Directories could be found!');
+	}
+
+	return directories;
+}
