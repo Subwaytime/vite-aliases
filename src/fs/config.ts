15cc0384fd6835222615673ffbe219becd9f36f0
Subwaytime <leon.l@nophase.de>
2021-08-02 17:27:09 +0200

    Update Config Read Process

--- src/fs/config.ts
+++ src/fs/config.ts
@@ -1,9 +1,9 @@
+import { createScanner, parse } from 'jsonc-parser';
+import { empty, slash, terminal } from '../utils';
 import { existsSync, readFileSync, writeFile } from 'fs';
-import { slash, terminal } from '../utils';
 
 import type { Generator } from '../generator';
 import { IDEConfig } from '../constants';
-import { parse } from 'jsonc-parser';
 
 /**
  * Creates a JS or TS Configfile
@@ -17,12 +17,21 @@ export function writeConfig(gen: Generator) {
 	}
 
 	const name = useTypescript ? 'tsconfig' : 'jsconfig';
-	const file = slash(`${root}/${name}.json`);
-
+	let file = slash(`${root}/${name}.json`);
 	let json;
 
 	if(existsSync(file)) {
-		json = parse(readFileSync(`${file}`).toString());
+		file = readFileSync(`${file}`).toString();
+		let errors: any[] = [];
+		json = parse(file, errors, { disallowComments: true });
+
+		if(!empty(errors)) {
+			throw new Error(
+				`Can't read JSON Config File, if it contains Comments please remove them.
+				For more Information: https://github.com/Subwaytime/vite-aliases/issues/25`,
+			);
+		}
+
 		if(json.compilerOptions) {
 			const paths = json.compilerOptions.paths || {};
 			json.compilerOptions.paths = { ...paths, ...gen.configPaths };
