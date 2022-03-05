f36b35e2704993c2179b22d76d019e75755520ca
Leon L <mail@lamixdesign.de>
2022-03-05 21:13:11 +0100

    add pre-enforcing to vite for better performance and less plugin incompatabilities

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import type { Plugin } from 'vite';
 import { Generator } from './generator';
 import type { Options } from './types';
+import type { Plugin } from 'vite';
 
 export function ViteAliases(options?: Partial<Options>): Plugin {
 	let gen: Generator;
 
 	return {
 		name: 'vite-aliases',
+		enforce: 'pre',
 		config(config, { command }) {
 			gen = new Generator(command, options);
-			gen.glob();
+			gen.init();
 
 			config.resolve = {
 				alias: gen.aliases
