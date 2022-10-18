36918ed0eade590857f367008e6ed7cc04237a24
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2022-10-18 23:22:24 +0700

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from 'tsup';
 
 export const tsup: Options = {
 	entry: ['src/index.ts'],
-	format: ['esm'],
+	format: ['cjs','esm'],
 	target: "node16",
 	dts: true,
 	splitting: true,
