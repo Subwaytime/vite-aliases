edf55d132ed05f70c717694198d9ca9011f5579a
Subwaytime <leon.l@nophase.de>
2023-01-04 20:57:14 +0100

    update tsup build configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,13 @@
 import type { Options } from 'tsup';
 
 export const tsup: Options = {
-	entry: ['src/index.ts'],
+	entry: ['./src/index.ts'],
 	format: ['esm'],
-	target: "node16",
+	target: 'node16',
+	// minify: 'terser',
 	dts: true,
 	splitting: true,
 	clean: true,
 	shims: false,
+	sourcemap: true
 };
