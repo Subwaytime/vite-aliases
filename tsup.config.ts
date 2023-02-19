956198b75309f27529bf01d26e98646242fbce99
Subwaytime <leon.l@nophase.de>
2023-02-19 16:32:52 +0100

    update tsup.config.ts
    add minify
    add shims
    remove sourcemap
    remove target

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,10 @@ import type { Options } from 'tsup';
 export const tsup: Options = {
 	entry: ['./src/index.ts'],
 	format: ['esm'],
-	target: 'node16',
-	// minify: 'terser',
+	minify: 'terser',
 	dts: true,
 	splitting: true,
 	clean: true,
-	shims: false,
-	sourcemap: true
+	shims: true,
+	treeshake: 'smallest'
 };
